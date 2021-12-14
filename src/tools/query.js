c52e41e3384c12a4f21ff6bd306333994c23bc1e
Guillaume Gagnaire <contact@scrola.net>
2021-12-14 09:39:40 +0100

    Handle fields

--- src/tools/query.js
+++ src/tools/query.js
@@ -1,13 +1,66 @@
 import LatLng from '../types/LatLng'
 
-export default function (request, collection) {
+function parseFields (fields) {
+  const words = []
+  let subs = []
+  let currentWord = ''
+  let opened = 0
+
+  for (let i = 0; i < fields.length; i++) {
+    if (fields[i] === ')') {
+      if (currentWord.length) {
+        words.push({ field: currentWord, sub: [] })
+        currentWord = ''
+      }
+      return words
+    } else if (fields[i] === '(') {
+      subs = parseFields(fields.substr(i + 1))
+      if (currentWord.length) {
+        words.push({ field: currentWord, sub: subs })
+      } else {
+        for (let sub of subs) {
+          words.push(sub)
+        }
+      }
+      currentWord = ''
+      opened = 1
+      for (let j = i + 1; j < fields.length; j++) {
+        if (fields[j] === '(') opened++
+        else if (fields[j] === ')') {
+          opened--
+          if (opened === 0) {
+            i = j + 1
+          }
+        }
+      }
+      if (opened) i = fields.length
+    } else if (fields[i] === ',') {
+      if (currentWord.length) {
+        words.push({ field: currentWord, sub: [] })
+        currentWord = ''
+      }
+    } else {
+      currentWord += fields[i]
+    }
+  }
+
+  if (currentWord.length) {
+    words.push({ field: currentWord, sub: [] })
+    currentWord = ''
+  }
+  return words
+}
+
+export default function (request, collection = null) {
   let page = 1
   let limit = 20
   let query = []
   let search = null
   let sort = undefined
   let geoField = null
-  let geoFields = collection.getGeoFields()
+  let geoFields = collection ? collection.getGeoFields() : []
+  const searchFields = collection ? collection.getSearchFields() : []
+  let filter = null
   let o = {}
 
   for (let key in request.query) {
@@ -25,10 +78,13 @@ export default function (request, collection) {
       case 'sort':
         sort = val[0]
         break
+      case 'fields':
+        filter = parseFields(val[0])
+        break
       case 'search':
         if (search === null) search = []
         for (const searchQuery of val) {
-          for (const searchField of collection.getSearchFields()) {
+          for (const searchField of searchFields) {
             o = {}
             o[searchField] = {
               $regex: searchQuery,
@@ -39,7 +95,7 @@ export default function (request, collection) {
         }
         break
       case 'geoNear':
-        if (!collection.canBeGeoSearched())
+        if (!collection || !collection.canBeGeoSearched())
           throw new Error('This collection cannot be geo searched.')
 
         val = val[0]
@@ -77,7 +133,7 @@ export default function (request, collection) {
         query.push(o)
         break
       case 'geoWithin':
-        if (!collection.canBeGeoSearched())
+        if (!collection || !collection.canBeGeoSearched())
           throw new Error('This collection cannot be geo searched.')
 
         val = val[0]
@@ -203,5 +259,5 @@ export default function (request, collection) {
     }
   }
 
-  return { page, limit, query, sort }
+  return { page, limit, query, sort, filter }
 }
