ebfa523653a4b856b6abf636b2e34f0023cdefd0
Guillaume Gagnaire <contact@scrola.net>
2021-12-19 21:33:42 +0100

    Handle different bool formats

--- src/types/Boolean.js
+++ src/types/Boolean.js
@@ -1,6 +1,25 @@
 import Base from './Base'
 
 export default class Boolean extends Base {
+  static in (value) {
+    switch (value) {
+      case true:
+      case 1:
+      case 'yes':
+      case 'y':
+      case 'true':
+        return true
+      case false:
+      case 0:
+      case 'no':
+      case 'n':
+      case 'false':
+        return false
+    }
+
+    return !!value
+  }
+
   static getMongooseType () {
     return Boolean
   }
